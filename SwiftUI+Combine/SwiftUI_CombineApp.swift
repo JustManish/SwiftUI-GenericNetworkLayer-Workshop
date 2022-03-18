7d75e4b8981262d97d662f52fd9e4a5c6a3a9a25
Manish <manish.patidar.97540@gmail.com>
2022-03-18 18:48:22 +0530

    Initial Commit

---
+++ SwiftUI+Combine/SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CombineApp.swift
+//  SwiftUI+Combine
+//
+//  Created by Manish Patidar on 18/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
